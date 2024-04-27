f36b9da1bd45500713316925013602ebbb93ad14
Sergey Pjankov <no.regret.ccg@gmail.com>
2024-04-27 19:50:51 +0300

    bug fix

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -7,13 +7,13 @@ import { Skeleton } from "../../components/Skeleton/Skeleton"
 
 export const Main = () => {
     const [news, setNews] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                setIsLoading(true);
                 const responce = await getNews();
                 setNews(responce.news);
+                setIsLoading(false);
             } catch (error) {
                 console.log(error)
             }
